187ae816dcc7c0fef91018a11c2b3fa09cd1b992
zakariyaeELKHALDI <81492191+zakariyaeELKHALDI@users.noreply.github.com>
2022-01-27 16:22:29 +0100

    geo.py
    Adding the new file geo.py including the solution of exercice1 part4 to the the new branch devel_000, which is based on the main branch

---
+++ geo.py
@@ -0,0 +1,8 @@
+class square():
+    def __init__(self, length):
+        self.length = length
+    def area(self):
+        return (self.length**2)
+    def perimeter(self):
+        return (self.length*4)
+    pass
